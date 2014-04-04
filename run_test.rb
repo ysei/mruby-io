2ed1a335cc7cdfa5e607331b982d78d43ac1082a
Masaki Muranaka <monaka@monami-ya.jp>
2014-04-05 07:09:35 +0900

    Add matrix build support.

--- run_test.rb
+++ run_test.rb
@@ -3,13 +3,17 @@
 # mrbgems test runner
 #
 
+gemname = File.basename(File.dirname(File.expand_path __FILE__))
+
 if __FILE__ == $0
-  repository, dir = 'https://github.com/mruby/mruby.git', 'tmp/mruby'
+  repository, branch, dir = ENV['MRUBY_REPO'], ENV['MRUBY_BRANCH'], 'tmp/mruby'
+
   build_args = ARGV
+  build_args = ['all', 'test']  if build_args.nil? or build_args.empty?
 
   Dir.mkdir 'tmp'  unless File.exist?('tmp')
   unless File.exist?(dir)
-    system "git clone #{repository} #{dir}"
+    system "git clone --depth 1 -b #{branch} #{repository} #{dir}"
   end
 
   exit system(%Q[cd #{dir}; MRUBY_CONFIG=#{File.expand_path __FILE__} ruby minirake #{build_args.join(' ')}])
